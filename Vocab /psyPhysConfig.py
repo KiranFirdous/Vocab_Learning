360a7239d0d8c7022c7b38cc2f20d3d967594162
KiranFirdous <87569729+KiranFirdous@users.noreply.github.com>
2025-07-21 08:14:55 +0100

    Create psyPhysConfig.py

---
+++ Vocab /psyPhysConfig.py
@@ -0,0 +1,24 @@
+switch to ruff
+from os import path as ospath
+clSource = '/home/colliculus/behaviourBoxes/software'
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    clSource = '/home/jan/Nextcloud/Documents/jan/behavbox' 
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    clSource = 'c:/Nextcloud/Documents/jan/behavbox' 
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    clSource = 'c:/Users/jan/Nextcloud/Documents/jan/behavbox' 
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    clSource = 'c:/users/colliculus'
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    clSource = 'c:/jan/behavbox'
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    clSource = 'd:/behavbox'
+if not ospath.exists(clSource+'/ratCageProgramsV3/ClickTrainLibrary.py'):
+    raise Exception('No valid path to ratCageProgramsV2 and 3 libraries')
+from sys import path as syspath
+syspath.append(clSource+'/ratCageProgramsV3')
+
+
+location='Freiburg'
+TDTcircuit=None
+RXdevice="RZ6"
